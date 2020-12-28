7a83a86388c5ddcae0d715e94af0cab6946251d8
Tihor Nezic <tihor.nezic@gmail.com>
2020-12-28 12:54:04 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#banner-area .owl-carousel").owlCarousel({
+        dots: true,
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 4000
+    });
+});
