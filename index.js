0c0693a2b88cf2ec9d49521c7686c15048bb3723
Tihor Nezic <tihor.nezic@gmail.com>
2020-12-30 14:20:36 +0100

    Top-Sale-Special-Price

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+    // banner owl carousel
     $("#banner-area .owl-carousel").owlCarousel({
         dots: true,
         items: 1,
@@ -6,4 +8,39 @@ $(document).ready(function () {
         autoplay: true,
         autoplayTimeout: 4000
     });
+
+    // top sale owl carousel
+
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        responsive: {
+            // for window greater than 0 viewport
+            0: {
+                items: 1
+            },
+            // for window greater than 600 vp
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+
+
+    // isotope filter
+    var $grid = $(".grid").isotope({
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows'
+    });
+
+    // filter items on button click
+    $(".button-group").on("click", "button", function(){
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({filter: filterValue});
+    });
+
 });
